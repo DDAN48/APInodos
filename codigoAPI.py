affddce2996f789741187dbc63e0f32674379d4c
Daniel Villarroel <ddan48@gmail.com>
2023-02-20 00:24:50 -0300

    daniel@DESKTOP-SRR09UG MINGW64 ~/Documents/DataEngineering/API $ git init Initialized empty Git repository in C:/Users/daniel/Documents/DataEngineering/AP I/.git/
    daniel@DESKTOP-SRR09UG MINGW64 ~/Documents/DataEngineering/API (master)
    $ git remote add origin https://github.com/DDAN48/APInodos.git
    
    daniel@DESKTOP-SRR09UG MINGW64 ~/Documents/DataEngineering/API (master)
    $ git add add
    fatal: pathspec 'add' did not match any files
    
    daniel@DESKTOP-SRR09UG MINGW64 ~/Documents/DataEngineering/API (master)
    $ git add codigoAPI.PY
    
    daniel@DESKTOP-SRR09UG MINGW64 ~/Documents/DataEngineering/API (master)
    $ git commit
    On branch master
    
    Initial commit
    
    Untracked files:
      (use "git add <file>..." to include in what will be committed)
            codigoAPI.py
    
    nothing added to commit but untracked files present (use "git add" to track)
    
    daniel@DESKTOP-SRR09UG MINGW64 ~/Documents/DataEngineering/API (master)
    $ git add ^C
    
    daniel@DESKTOP-SRR09UG MINGW64 ~/Documents/DataEngineering/API (master)
    $ git add
    
    daniel@DESKTOP-SRR09UG MINGW64 ~/Documents/DataEngineering/API
    $ git init
    Initialized empty Git repository in C:/Users/daniel/Documents/DataEngineering/AP
    I/.git/
    
    daniel@DESKTOP-SRR09UG MINGW64 ~/Documents/DataEngineering/API (master)
    $ git remote add origin https://github.com/DDAN48/APInodos.git
    
    daniel@DESKTOP-SRR09UG MINGW64 ~/Documents/DataEngineering/API (master)
    $ git add add
    fatal: pathspec 'add' did not match any files
    
    daniel@DESKTOP-SRR09UG MINGW64 ~/Documents/DataEngineering/API (master)
    $ git add codigoAPI.PY
    
    daniel@DESKTOP-SRR09UG MINGW64 ~/Documents/DataEngineering/API (master)
    $ git commit
    On branch master
    
    Initial commit
    
    Untracked files:
      (use "git add <file>..." to include in what will be committed)
            codigoAPI.py
    
    nothing added to commit but untracked files present (use "git add" to track)
    
    daniel@DESKTOP-SRR09UG MINGW64 ~/Documents/DataEngineering/API (master)
    $ git add ^C
    
    daniel@DESKTOP-SRR09UG MINGW64 ~/Documents/DataEngineering/API (master)
    $ git add
    daniel@DESKTOP-SRR09UG MINGW64 ~/Documents/DataEngineering/API
    $ git init
    Initialized empty Git repository in C:/Users/daniel/Documents/DataEngineering/AP
    I/.git/
    
    daniel@DESKTOP-SRR09UG MINGW64 ~/Documents/DataEngineering/API (master)
    $ git remote add origin https://github.com/DDAN48/APInodos.git
    
    daniel@DESKTOP-SRR09UG MINGW64 ~/Documents/DataEngineering/API (master)
    $ git add add
    fatal: pathspec 'add' did not match any files
    
    daniel@DESKTOP-SRR09UG MINGW64 ~/Documents/DataEngineering/API (master)
    $ git add codigoAPI.PY
    
    daniel@DESKTOP-SRR09UG MINGW64 ~/Documents/DataEngineering/API (master)
    $ git commit
    On branch master
    
    Initial commit
    
    Untracked files:
      (use "git add <file>..." to include in what will be committed)
            codigoAPI.py
    
    nothing added to commit but untracked files present (use "git add" to track)
    
    daniel@DESKTOP-SRR09UG MINGW64 ~/Documents/DataEngineering/API (master)
    $ git add ^C
    
    daniel@DESKTOP-SRR09UG MINGW64 ~/Documents/DataEngineering/API (master)
    $ git add

---
+++ codigoAPI.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify, request
+import pyodbc
+import pymysql
+
+app = Flask(__name__)
+
+# Configuración de la conexión a SQL Server
+server = 'us-east.connect.psdb.cloud'
+databases = 'poligonostlc'
+
+username = '0cv28h5f1h1csdel4y6p'
+clave = 'pscale_pw_17erBbPBTK1q3dPc7ZhnmgFE52sgZ2s02qUBrGeAMxG'
+
+conn=pymysql.connect(user=username, password=clave,host=server,  database=databases,port=3306,ssl={'ssl': {'ca': 'path/to/cert.pem'}})
+"""cursor=conn.cursor()"""
+
+'''sql_query = "select * from poligonos where nodo='PI05A' ;"
+cursor.execute(sql_query)
+datos=cursor.fetchall()
+
+print(datos[0][1])'''
+
+
+# Endpoint para obtener la zona dado el nodo
+@app.route('/poligonos/<string:nodo>/zona', methods=['GET'])
+def get_user_age(nodo):
+    nod='PI05A'
+    cursor = conn.cursor()
+    sql_query = f'select * from poligonos where nodo="{nodo}";'
+    '''f'select * from poligonos WHERE nodo={nod};'''
+    cursor.execute(sql_query)
+    zona = cursor.fetchall()
+    cursor.close()
+    if zona:
+        return jsonify({'zona': zona[0][1]})
+    else:
+        return jsonify({'error': 'Usuario no encontrado'})
+
+if __name__ == '__main__':
+    app.run()
+
+
