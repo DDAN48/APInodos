57ce7f1bcb00c7f4131692ea7b146e68520884d8
Daniel Villarroel <ddan48@gmail.com>
2023-02-26 15:10:37 -0300

    se agrega Cors permisos

--- codigoAPI.py
+++ codigoAPI.py
@@ -1,12 +1,17 @@
 from flask import Flask, jsonify, request
 import pyodbc
 import pymysql
-from credenciales from server
+from credenciales import server,clave,username,databases
+
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) # habilita CORS para la aplicación Flask
+
 
 # Configuración de la conexión a SQL Server
 conn=pymysql.connect(user=username, password=clave,host=server,  database=databases,port=3306,ssl={'ssl': {'ca': 'path/to/cert.pem'}})
+
 
 @app.route('/ping')
 def ping():
@@ -15,7 +20,7 @@ def ping():
 # Endpoint para obtener la zona dado el nodo
 @app.route('/poligonos/<string:nodo>/zona', methods=['GET'])
 def get_user_age(nodo):
-    nod='PI05A'
+   
     cursor = conn.cursor()
     sql_query = f'select * from poligonos where nodo="{nodo}";'
     '''f'select * from poligonos WHERE nodo={nod};'''
@@ -26,6 +31,28 @@ def get_user_age(nodo):
         return jsonify({'zona': zona[0][1]})
     else:
         return jsonify({'error': 'Usuario no encontrado'})
+
+
+# Endpoint para obtener NODO , PARTIDO , JEFATURA del cliente
+@app.route('/cl', methods=['POST'])
+def get_nodo_partido():
+      
+    cliente=request.get_json()['idcliente'] #si el id se de consulta entra por json, si el id de consuta entra por form-data usar request.form['id_cliente']
+
+    cursor = conn.cursor()
+    sql_query = f'select * from clientes where idcliente="{cliente}";'
+    cursor.execute(sql_query)
+    datos = cursor.fetchall()
+    cursor.close()
+    print(datos)
+    if datos:
+        return jsonify({'Partido': datos[0][1], 
+                         'Nodo':datos[0][3],
+                         'Jefatura':datos[0][4],
+        })
+    else:
+        return jsonify({'error': 'Usuario no encontrado'})
+
 
 
 
